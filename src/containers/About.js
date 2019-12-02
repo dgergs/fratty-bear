1fb221a9a8d0b86f9ddfac20e88def2cb75606c3
Drey William Gerger <dgerger@berkeley.edu>
2019-12-02 11:43:32 -0800

    updated about us page

--- src/containers/About.js
+++ src/containers/About.js
@@ -43,12 +43,8 @@ const styles = {
     },
 }
 
-const yikes = 'Back in 2017 when I was elected as Rush Officer of Pi Lambda Phi at the University of California, Berkeley, I had the responsibility of designing and attaining the rush shirts for the semester. Having already been making custom t-shirts for a wholesale company I started in high school, my chapter was naturally okay purchasing them from me. I saw this as a business opportunity and began selling custom designed t-shirts to many of the houses on Berkeley’s Greek Row. Through discussions with my friends and customers, I was able to understand the frustrations that come from the process of ordering custom merchandise from finding a design that everyone can agree on to wondering whether the “Gildan 2000” t-shirts a greek apparel website recommended are going to feel high quality or cheap. I did my research and realized that there are very few options that fraternities guys can choose from, meaning they have to settle with what’s available. What really surprised me was how much these other websites were charging for what should cost a lot less outside of the greek industry. A navy shirt that would normally cost anyone else 15 dollars now costs you 22 dollars because you are a college student in greek life that “has money and can pay more”. Doesn’t sounds fair, does it? That’s because its not. With the guidance and encouragement from my business professor, my partner Drey and I decided to focus our attention on solving these issues and simplifying the process of ordering your boys the dope shirts that they deserve. And that’s how Fratty Bear began.'
-const info = 'It wasn\'t fate. It’s not a calling, vocation, or anything that dramatic. There wasn\'t a parting of the heavens or some divine oracle declaring that our life\'s purpose was to make custom clothing. Nahhhhh... Nothing like that. We\'re just two college students tired of seeing fraternities get overcharged for personalized clothing.'
-const info2 = 'We have the machinery to do embroidery, direct-to-garment printing, silk screen, and full color vinyl heat transfer. We can make your clothing exactly the way you want it.'
-
-const abt0 = 'It\'s really not that crazy. Fraternities shouldn\'t be over charged for custom apparel.' 
-const abt1 = 'We started Fratty Bear because we wanted to make ordering fraternity apparel easy. No hidden fees, no unreasonable order restrictions, and no more ugly apparel. We do it all from shirts, to hoodies, to banners, and more.'
+const abt0 = 'Here at Fratty Bear, we’re in the business of creating something great. We value honesty, loyalty, and experiences worth talking about. Our top priorities are making high quality products and giving unbeatable customer service to each and every person that steps through our doors. We aim to provide the best possible experience that you can’t help but tell your friends about the guys at Fratty Bear.' 
+const abt1 = ' \"Don\'t find customers for your products, find products for your customers.\" – Seth Godin'
 const abt2 = ''
 
 var link = <a href='https://dreygerger.typeform.com/to/CTpPof'>here</a>;
@@ -68,7 +64,7 @@ class About extends React.Component {
                     <div style={styles.infoContainer} >
                       <p style={styles.infoText} >{abt0}</p>
                       <p style={styles.infoText} >{abt1}</p>
-                      <p style={styles.infoText} >Don’t be afraid to reach out! Contact us here or email us at <a href='mailto:contact@frattybear.com'>contact@frattybear.com</a>.</p>
+                      <p style={styles.infoText} >Please reach out! Email us at <a href='mailto:contact@frattybear.com'>contact@frattybear.com</a></p>
                     </div>
                 </div>
             </div>
