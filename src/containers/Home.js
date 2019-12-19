7f64a2e3631060a6fbcc248994096979ec74f00e
Drey William Gerger <dgerger@berkeley.edu>
2019-12-19 13:38:28 -0800

    got basic google analytics working

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
+import ReactGA from 'react-ga';
 import FrattyBear from '../components/FrattyBear';
 import ImageSeries from '../components/ImageSeries';
 import Button from '@material-ui/core/Button';
@@ -74,73 +75,75 @@ const styles = {
 
 const images = [require('../images/stock_1.jpg'), require('../images/stock_2.jpg'), require('../images/stock_3.jpg'), require('../images/stock_4.jpg')];
 
+
 class Home extends React.PureComponent {
-    render() {
-        return (
-            <div>
-              <div style={styles.bearContainer}>
-                <FrattyBear />
-                <h1 style={styles.landingHeader}>Custom Fraternity Apparel</h1>
-                <Button variant="contained" href='https://frattybear.typeform.com/to/Cw3MGa'>Get in touch</Button>
-              </div>
-              <h1 style={styles.title}>What We Offer</h1>
-              <div style={styles.value}>
-                <div style={styles.valueProp}>
-                  <img alt="" src={require('../images/tshirt_logo.png')} style={styles.valueImg} />
-                  <h3>Clothing Variety</h3>
-                  <p>We've got you covered with shirts, hoodies, hats, or anything else you need.</p>
-                </div>
-                <div style={styles.valueProp}>
-                  <img alt="" src={require('../images/FrattyBearIcon.png')} style={styles.valueImg} />
-                  <h3>Loyalty Program</h3>
-                  <p>Earn rewards your house can enjoy through shopping with us.</p>
-                </div>
-                <div style={styles.valueProp}>
-                  <img alt="" src={require('../images/charity_logo.svg')} style={styles.valueImg} />
-                  <h3>Philanthropy</h3>
-                  <p>For every philanthropy shirt order, we donate 5% of your order to a non-profit of your choice!</p>
-                </div>
-              </div>
-              <ImageSeries images={images}/>
-              <h1 style={styles.title}>Past Clients</h1>
-              <div style={{textAlign: 'center', overflowWrap: 'break-word', flex: 1}}>
-                <TextLoop interval={5000} mask = {true} noWrap = {window.innerWidth > 500}>
-                  <div style={styles.container} style={{flexDirection:'column', textAlign: 'center',}}>
-                    <div style={styles.quotes}>
-                      "Shirts arrived and they look GREAT"
-                    </div>
-                    <div style={styles.writtenBy}>
-                      - Max, UC Berkeley
-                    </div>
-                  </div>
-                  <div style={styles.container} style={{flexDirection:'column', textAlign: 'center',}}>
-                    <div style={styles.quotes}>
-                      "Fantastic. Huge hit"
-                    </div>
-                    <div style={styles.writtenBy}>
-                      - Alec, Columbia University
-                    </div>
-                  </div>
-                  <div style={styles.container} style={{flexDirection:'column', textAlign: 'center',}}>
-                    <div style={styles.quotes}>
-                      "Umm.. I'm not in a frat"
-                    </div>
-                    <div style={styles.writtenBy}>
-                      - Jen, UC Berkeley
-                    </div>
-                  </div>
-                  <div style={styles.container} style={{flexDirection:'column', textAlign: 'center',}}>
-                    <div style={styles.quotes}>
-                      "These philo shirts put our house on the map"
-                    </div>
-                    <div style={styles.writtenBy}>
-                      - Ken, Arizona State University
-                    </div>
-                  </div>
-                </TextLoop>{" "}
-                </div>
+  
+  render() {
+      return (
+          <div>
+            <div style={styles.bearContainer}>
+              <FrattyBear />
+              <h1 style={styles.landingHeader}>Custom Fraternity Apparel</h1>
+              <Button variant="contained" href='https://frattybear.typeform.com/to/Cw3MGa'>Get in touch</Button>
             </div>
-          );
+            <h1 style={styles.title}>What We Offer</h1>
+            <div style={styles.value}>
+              <div style={styles.valueProp}>
+                <img alt="" src={require('../images/tshirt_logo.png')} style={styles.valueImg} />
+                <h3>Clothing Variety</h3>
+                <p>We've got you covered with shirts, hoodies, hats, or anything else you need.</p>
+              </div>
+              <div style={styles.valueProp}>
+                <img alt="" src={require('../images/FrattyBearIcon.png')} style={styles.valueImg} />
+                <h3>Loyalty Program</h3>
+                <p>Earn rewards your house can enjoy through shopping with us.</p>
+              </div>
+              <div style={styles.valueProp}>
+                <img alt="" src={require('../images/charity_logo.svg')} style={styles.valueImg} />
+                <h3>Philanthropy</h3>
+                <p>For every philanthropy shirt order, we donate 5% of your order to a non-profit of your choice!</p>
+              </div>
+            </div>
+            <ImageSeries images={images}/>
+            <h1 style={styles.title}>Past Clients</h1>
+            <div style={{textAlign: 'center', overflowWrap: 'break-word', flex: 1}}>
+              <TextLoop interval={5000} mask = {true} noWrap = {window.innerWidth > 500}>
+                <div style={styles.container} style={{flexDirection:'column', textAlign: 'center',}}>
+                  <div style={styles.quotes}>
+                    "Shirts arrived and they look GREAT"
+                  </div>
+                  <div style={styles.writtenBy}>
+                    - Max, UC Berkeley
+                  </div>
+                </div>
+                <div style={styles.container} style={{flexDirection:'column', textAlign: 'center',}}>
+                  <div style={styles.quotes}>
+                    "Fantastic. Huge hit"
+                  </div>
+                  <div style={styles.writtenBy}>
+                    - Alec, Columbia University
+                  </div>
+                </div>
+                <div style={styles.container} style={{flexDirection:'column', textAlign: 'center',}}>
+                  <div style={styles.quotes}>
+                    "Umm.. I'm not in a frat"
+                  </div>
+                  <div style={styles.writtenBy}>
+                    - Jen, UC Berkeley
+                  </div>
+                </div>
+                <div style={styles.container} style={{flexDirection:'column', textAlign: 'center',}}>
+                  <div style={styles.quotes}>
+                    "These philo shirts put our house on the map"
+                  </div>
+                  <div style={styles.writtenBy}>
+                    - Ken, Arizona State University
+                  </div>
+                </div>
+              </TextLoop>{" "}
+              </div>
+          </div>
+        );
     }
 
 }
