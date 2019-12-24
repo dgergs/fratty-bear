282a2777dc816f7cd6be86292b39db89577c91d5
Drey William Gerger <dgerger@berkeley.edu>
2019-12-23 17:09:45 -0800

    Updates

---
+++ precache-manifest.cd296fc936b31cce93282fb7934229ae.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29614b28fa9d4c38fafc734e04708862",
+    "url": "dgergs.github.io/fratty_bear/index.html"
+  },
+  {
+    "revision": "abeba85e8bece82a6d2d",
+    "url": "dgergs.github.io/fratty_bear/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5dd1498ce4669805cae5",
+    "url": "dgergs.github.io/fratty_bear/static/css/main.1f2767f6.chunk.css"
+  },
+  {
+    "revision": "abeba85e8bece82a6d2d",
+    "url": "dgergs.github.io/fratty_bear/static/js/2.2f936b8c.chunk.js"
+  },
+  {
+    "revision": "5dd1498ce4669805cae5",
+    "url": "dgergs.github.io/fratty_bear/static/js/main.2f04d31a.chunk.js"
+  },
+  {
+    "revision": "4952c183113297c8c327",
+    "url": "dgergs.github.io/fratty_bear/static/js/runtime~main.1a6a1e39.js"
+  },
+  {
+    "revision": "6dfb9bc903e79cfaeea7eb50a4c572b2",
+    "url": "dgergs.github.io/fratty_bear/static/media/FrattyBearIcon.6dfb9bc9.png"
+  },
+  {
+    "revision": "2d33147c73768c95c610cbdb21cd7b2f",
+    "url": "dgergs.github.io/fratty_bear/static/media/LoyaltyTemplate.2d33147c.png"
+  },
+  {
+    "revision": "b03682202ae64a9b38e05e1b1fb8079e",
+    "url": "dgergs.github.io/fratty_bear/static/media/aboutus2.b0368220.jpg"
+  },
+  {
+    "revision": "4a4f0fc4defcb7d2d9330c8ad0c07229",
+    "url": "dgergs.github.io/fratty_bear/static/media/charity_logo.4a4f0fc4.svg"
+  },
+  {
+    "revision": "5dce6878c6571183ad00b2a62de1e03d",
+    "url": "dgergs.github.io/fratty_bear/static/media/peteTheBear.5dce6878.jpg"
+  },
+  {
+    "revision": "1f272f93baf8a24d422823fe7f647ef8",
+    "url": "dgergs.github.io/fratty_bear/static/media/stock_1.1f272f93.jpg"
+  },
+  {
+    "revision": "9c7496f7c2baff23940c7f431770ffaa",
+    "url": "dgergs.github.io/fratty_bear/static/media/stock_2.9c7496f7.jpg"
+  },
+  {
+    "revision": "5f32c20bbd639f5f9c83e88b16a72a29",
+    "url": "dgergs.github.io/fratty_bear/static/media/stock_3.5f32c20b.jpg"
+  },
+  {
+    "revision": "0f1228a31567f3bdd168ea8f01db05e5",
+    "url": "dgergs.github.io/fratty_bear/static/media/stock_4.0f1228a3.jpg"
+  }
+]);
